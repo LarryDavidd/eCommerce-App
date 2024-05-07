f182dcbd94ede2275eda0e8d73f412232fd9e113
Airat2000 <gerasimovevgeni16082003@gmail.com>
2024-05-08 02:04:50 +0400

    feat: add header

---
+++ src/shared/ui-kit/Buttons/ButtonIconCounter/ButtonIconCounter.vue
@@ -0,0 +1,59 @@
+<script lang="ts" setup>
+type PropsType = {
+  count: number;
+};
+
+const props = withDefaults(defineProps<PropsType>(), {
+  count: 0
+});
+</script>
+
+<template>
+  <div class="button-icon">
+    <slot name="icon" />
+
+    <div class="button-icon__info">
+      <span>{{ props.count }}</span>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.button-icon {
+  @apply relative 
+  flex
+  w-fit
+  cursor-pointer 
+  select-none 
+  items-center 
+  justify-center
+  rounded-lg
+  duration-500;
+
+  &:hover {
+    background-color: rgb(110, 110, 118);
+  }
+
+  &__info {
+    position: absolute;
+    left: 60%;
+    bottom: 40%;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    width: 16px;
+    height: 16px;
+
+    background-color: rgba(255, 0, 0, 0.5);
+    border-radius: 100%;
+
+    font-size: 1rem;
+    font-weight: bold;
+    color: white;
+
+    backdrop-filter: contrast(0);
+  }
+}
+</style>
--- src/widgets/MainHeader/ui/MainHeader.vue
+++ src/widgets/MainHeader/ui/MainHeader.vue
@@ -1,8 +1,69 @@
 <script lang="ts" setup>
-import { SearchBar } from '@shared/ui-kit/SearchBar/SearchBar.vue';
+import SearchBar from '@shared/ui-kit/SearchBar/SearchBar.vue';
+import ButtonIconCounter from '@shared/ui-kit/Buttons/ButtonIconCounter/ButtonIconCounter.vue';
+import ButtonIcon from '@shared/ui-kit/Buttons/ButtonIcon/ButtonIcon.vue';
+import LikeHeart from '@shared/ui-kit/Icons/LikeHeart.vue';
+import CartIcon from '@/shared/ui-kit/Icons/CartIcon.vue';
+import LoginIcon from '@/shared/ui-kit/Icons/LoginIcon.vue';
 </script>
 
 <template>
-  <SearchBar></SearchBar>
-  <div>header</div>
+  <header class="flex h-16 w-full flex-col justify-center">
+    <div class="header__wrapper mx-20 flex justify-between">
+      <div>Shop Name</div>
+      <SearchBar></SearchBar>
+
+      <div class="flex justify-between gap-10">
+        <div class="login__button flex gap-2">
+          <div class="login__button-text flex flex-col justify-center">Registration</div>
+          <ButtonIcon>
+            <template v-slot:icon>
+              <LoginIcon />
+            </template>
+          </ButtonIcon>
+        </div>
+
+        <div class="login__button flex gap-2">
+          <div class="login__button-text flex flex-col justify-center">Login</div>
+          <ButtonIcon>
+            <template v-slot:icon>
+              <LoginIcon />
+            </template>
+          </ButtonIcon>
+        </div>
+
+        <ButtonIconCounter>
+          <template v-slot:icon>
+            <LikeHeart />
+          </template>
+        </ButtonIconCounter>
+
+        <ButtonIconCounter>
+          <template v-slot:icon>
+            <CartIcon />
+          </template>
+        </ButtonIconCounter>
+      </div>
+    </div>
+  </header>
 </template>
+
+<style scoped lang="scss">
+header {
+  background: rgb(240, 240, 240);
+}
+
+.header__wrapper {
+  border: 20px;
+}
+
+.login__button-text {
+  color: rgb(82, 82, 82);
+  font-family: Montserrat;
+  font-size: 12px;
+  font-weight: 500;
+  line-height: 100%;
+  letter-spacing: 5%;
+  text-align: left;
+}
+</style>
