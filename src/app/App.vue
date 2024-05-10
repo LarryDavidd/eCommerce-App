9acd5b93b166bb6837334eda9c075f1d5c887db1
Polina Deniskina <polinaoioi67@gmail.com>
2024-05-10 13:31:11 +0300

    feat: add MainFooter component

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
 import { MainHeader } from '@widgets/MainHeader';
+import { MainFooter } from '@shared/components/MainFooter';
 </script>
 
 <template>
   <MainHeader />
   <RouterView />
+  <MainFooter />
 </template>
---
+++ src/shared/components/MainFooter/ui/FooterColumn.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+type PropsType = {
+  name: string;
+  links?: string[];
+};
+
+const props = defineProps<PropsType>();
+</script>
+
+<template>
+  <div class="max-col-with">
+    <h2 class="mb-4 max-w-xs text-left text-base font-medium leading-7 text-black">{{ props.name }}</h2>
+    <ul>
+      <li
+        class="mb-3 text-base leading-6 text-gray-900"
+        v-for="(link, index) in props.links"
+        :key="index"
+      >
+        <a href="#">{{ link }}</a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.max-col-with {
+  max-width: 250px;
+}
+</style>
