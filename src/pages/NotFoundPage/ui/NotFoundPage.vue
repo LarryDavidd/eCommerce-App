bca60eb3ad973333c660ca3435b8354883314d4c
Polina Deniskina <polinaoioi67@gmail.com>
2024-05-14 23:14:46 +0300

    feat: add page 404

---
+++ src/pages/NotFoundPage/ui/NotFoundPage.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import MainButton from '@shared/ui-kit/Buttons/MainButton/MainButton.vue';
+</script>
+
+<template>
+  <div class="mx-10 lg:mx-20">
+    <div class="test mb-4 flex w-full flex-col items-center sm:items-start">
+      <div class="test4 md:h-70vh flex w-full bg-contain bg-center bg-no-repeat py-5 text-center sm:bg-right-top sm:py-20 sm:text-left">
+        <div class="test6 flex flex-col justify-between gap-6 font-bold text-black sm:w-3/5 md:justify-center">
+          <p class="text-3xl md:text-5xl">Oops... Page not found :/</p>
+          <p class="text-1xl md:text-2xl">We are already working on fixing this error.</p>
+        </div>
+      </div>
+      <router-link to="/home">
+        <MainButton
+          name="return to homepage"
+          :options="{ buttonStyle: 'dark-grey' }"
+        />
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.test4 {
+  height: 23rem;
+  background-image: url('../../../shared/assets/images/clothes_hanger.png');
+}
+@media (max-width: 1024px) {
+  .test4 {
+    background-size: 25rem;
+  }
+}
+@media (max-width: 767px) {
+  .test4 {
+    background-size: 15rem;
+  }
+}
+</style>
