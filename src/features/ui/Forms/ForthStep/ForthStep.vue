fc3b059c54712d8bc131ba3d2cefa367df0d9882
SyrtcevaDaria <syrtceva566333@gmail.com>
2024-05-15 15:19:53 +0300

    fix: fix validation requirements

--- src/features/ui/Forms/ForthStep/ForthStep.vue
+++ src/features/ui/Forms/ForthStep/ForthStep.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import SimpleInput from '@shared/ui-kit/Inputs/SimpleInput/SimpleInput.vue';
 import { computed, ref, toRefs, watch } from 'vue';
-import { validateName, validatePostalCode } from '@shared/utils/validation';
+import { validateName, validatePostalCode, validateStreet } from '@shared/utils/validation';
 import FormWrapper from '@shared/ui-kit/FormWrapper/FormWrapper.vue';
 import SelectInput from '@shared/ui-kit/Inputs/SelectInput/SelectInput.vue';
 import CheckBox from '@shared/ui-kit/Inputs/CheckBox/CheckBox.vue';
@@ -30,7 +30,7 @@ watch(
   (newValue: string) => {
     emits('updateBilling');
     if (newValue) {
-      let result = validateName(newValue);
+      let result = validateStreet(newValue);
       const errorList = result.errors.map((error) => error.message).filter((message): message is string => message !== undefined);
       errorsStreet.value = errorList.length > 0 ? errorList : null;
     }
