99dc75a8322be8f92eeeb4668ce549ff35ac995b
Airat2000 <gerasimovevgeni16082003@gmail.com>
2024-06-08 22:14:07 +0400

    feat: add fn for create cart

---
+++ src/entities/Cart/api/fetchCart.ts
@@ -0,0 +1,48 @@
+import Client from '@shared/api/client/Client';
+import type { CartDraft } from '@commercetools/platform-sdk';
+
+class CartApi {
+  public async createCartForAnonymousCustomer(refresh_token: string, cartDraft: CartDraft) {
+    const client = Client.getInstance().clientWithRefreshTokenFlow(refresh_token);
+    return client
+      .me()
+      .carts()
+      .post({
+        body: cartDraft
+      })
+      .execute()
+      .then((data) => data.body)
+      .catch((err) => err);
+  }
+
+  public async getCart(refresh_token: string) {
+    const client = Client.getInstance().clientWithRefreshTokenFlow(refresh_token);
+    return client
+      .me()
+      .activeCart()
+      .get()
+      .execute()
+      .then((data) => data.body)
+      .catch((err) => err);
+  }
+
+  public async addProductToCart(cartId: string, cartVersion: number, productId: string) {
+    const client = Client.getInstance().anonymousClient;
+
+    return await client
+      .me()
+      .carts()
+      .withId({ ID: cartId })
+      .post({
+        body: {
+          version: cartVersion,
+          actions: [{ action: 'addLineItem', productId }]
+        }
+      })
+      .execute()
+      .then((data) => data.body)
+      .catch((err) => err);
+  }
+}
+
+export default CartApi;
--- src/entities/Costumer/store/costumerStore.ts
+++ src/entities/Costumer/store/costumerStore.ts
@@ -6,7 +6,6 @@ import type { Customer, CustomerDraft } from '@commercetools/platform-sdk';
 import { revokingToken } from '@/auth/api/revokeToken';
 import { useNotificationStore } from '@/shared/Store/AlertMessageStore';
 import router from '@/app/router';
-import type { Client, ClientResponse } from '@commercetools/sdk-client-v2';
 
 export const useCostumerStore = defineStore('costumer_store', () => {
   const costumerApi = new CostumerApi();
@@ -30,18 +29,20 @@ export const useCostumerStore = defineStore('costumer_store', () => {
     return costumerCredentials.value;
   });
 
-  const setTokenToLs = () => {
-    if (isLogined.value) {
-      if (userRefreshToken.value) useLocalStorage().set('refresh_token', userRefreshToken.value);
-      if (userAccessToken.value) useLocalStorage().set('access_token', userAccessToken.value);
-    }
-  };
-
   const setNotificationSucces = () => {
     const notification = {
       id: Date.now(),
       message: 'Login was successful' + ' ' + (isLogined.value ? 'Loginded user' : 'Anon user'),
       type: 'success'
+    };
+    alert.addNotification(notification);
+  };
+
+  const setNotificationError = (message: string) => {
+    const notification = {
+      id: Date.now(),
+      message: message,
+      type: 'error'
     };
     alert.addNotification(notification);
   };
@@ -53,12 +54,7 @@ export const useCostumerStore = defineStore('costumer_store', () => {
       isExist.value = true;
       setNotificationSucces();
     } else {
-      const notification = {
-        id: Date.now(),
-        message: 'Please reload page',
-        type: 'error'
-      };
-      alert.addNotification(notification);
+      setNotificationError(anonCostumer.message);
     }
 
     return { isExist };
@@ -78,11 +74,18 @@ export const useCostumerStore = defineStore('costumer_store', () => {
       }
     }
 
-    if (!isLogined.value) await AnonCostumer();
+    if (!isLogined.value && refresh_token) {
+      const existAnonCostumre = await costumerApi.refreshAnonCostumer(String(refresh_token));
+
+      if (existAnonCostumre?.statusCode === 200) {
+        isExist.value = true;
+        setNotificationSucces();
+      }
+    }
+
+    if (!isExist.value) await AnonCostumer();
 
     isLoading.value = false;
-
-    setTokenToLs();
   }
 
   async function LoginCostumer(email: string, password: string) {
@@ -94,30 +97,18 @@ export const useCostumerStore = defineStore('costumer_store', () => {
       isExist.value = true;
       isLogined.value = true;
       setNotificationSucces();
-    } else if (res.statusCode === 400) {
-      const notification = {
-        id: Date.now(),
-        message: res.message,
-        type: 'error'
-      };
-      alert.addNotification(notification);
     } else {
-      const notification = {
-        id: Date.now(),
-        message: 'Please try again or reload page',
-        type: 'error'
-      };
-      alert.addNotification(notification);
+      setNotificationError(res.message);
     }
-    isLoading.value = false;
 
-    setTokenToLs();
+    isLoading.value = false;
 
     return { isLogined };
   }
 
   async function RegistrationCostumer(draft: CustomerDraft) {
     isLoading.value = true;
+
     const res = await costumerApi.regCostumer(draft);
 
     if (res.statusCode === 201) {
@@ -128,14 +119,10 @@ export const useCostumerStore = defineStore('costumer_store', () => {
       };
       alert.addNotification(notification);
       if (draft.password) await LoginCostumer(draft.email, draft.password);
-    } else if (res.statusCode >= 300) {
-      const notification = {
-        id: Date.now(),
-        message: res.message,
-        type: 'error'
-      };
-      alert.addNotification(notification);
+    } else {
+      setNotificationError(res.message);
     }
+
     isLoading.value = false;
 
     return { isLogined };
@@ -153,7 +140,7 @@ export const useCostumerStore = defineStore('costumer_store', () => {
     isLogined.value = false;
 
     if (res instanceof Error) {
-      alert.addNotification({ status: 'error', message: 'Please reload page' });
+      setNotificationError(res.message);
     } else {
       AnonCostumer();
       router.push({ name: 'login' });
@@ -162,28 +149,9 @@ export const useCostumerStore = defineStore('costumer_store', () => {
     isLoading.value = false;
   }
 
-  const requestCostumer = async () => {
-    isLoading.value = true;
-
-    const refresh_token = ls.load('refresh_token');
-
-    if (refresh_token) {
-      const res = await costumerApi.refreshCostumer(String(refresh_token));
-
-      if (res instanceof Error) {
-        alert.addNotification({ status: 'error', message: 'Please reload page' });
-      } else {
-        costumerCredentials.value = res.body;
-      }
-    }
-
-    isLoading.value = false;
-  };
-
   return {
     getCostumerCredentials,
     getIsLoading,
-    requestCostumer,
     AnonCostumer,
     LoginExistigCostumer,
     LoginCostumer,
