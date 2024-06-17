061e5261bf8910a89ae5d383a953bae1622f1329
Airat2000 <gerasimovevgeni16082003@gmail.com>
2024-06-17 23:31:02 +0400

    feat: add pagination

--- src/entities/Product/store/productStore.ts
+++ src/entities/Product/store/productStore.ts
@@ -126,6 +126,12 @@ export const useProductStore = defineStore('product_store', () => {
     return Math.ceil(data.value.total / data.value.limit);
   });
 
+  const GetCurrentPaginationNum = computed(() => {
+    if (data.value === null) return 0;
+    if (data.value.offset === undefined) return 0;
+    return Math.ceil(data.value.offset / data.value.limit);
+  });
+
   // Actions
   const requestGetProduct = async (offset = 0, limit = 10) => {
     if (offset <= 0) offset = 0;
@@ -138,8 +144,7 @@ export const useProductStore = defineStore('product_store', () => {
     isLoading.value = false;
   };
 
-  const requestGetProductByQueryParams = async (offset = 0) => {
-    if (offset <= 0) offset = 0;
+  const requestGetProductByQueryParams = async () => {
     isLoading.value = true;
     const products = await productApi.fetchQueryProductProjectionsByQP();
 
@@ -160,34 +165,12 @@ export const useProductStore = defineStore('product_store', () => {
     return res.body;
   };
 
-  const requestGetProductsByCategory = async (ids: string[]) => {
-    isLoading.value = true;
-    const products = await productApi.fetchQueryProductProjectionsByCategory(ids);
-
-    if (products instanceof Error) return;
-
-    data.value = products;
-    isLoading.value = false;
-    return products;
-  };
-
-  const requestProductSearch = async (text: string) => {
-    isLoading.value = true;
-    const products = await productApi.fetchProductProjectionSearch(appState.getState.language, text);
-
-    if (products instanceof Error) return;
-
-    data.value = products;
-    isLoading.value = false;
-    return products;
-  };
-
   return {
     requestGetProduct,
     requestGetProductByQueryParams,
-    requestGetProductsByCategory,
-    requestProductSearch,
     requestGetProductById,
+    GetTotalPaginationNumber,
+    GetCurrentPaginationNum,
     GetProducts,
     GetProduct,
     isLoading
--- src/entities/Product/ui/ProductList.vue
+++ src/entities/Product/ui/ProductList.vue
@@ -3,8 +3,10 @@ import { onMounted } from 'vue';
 import { useProductStore } from '../store/productStore';
 import { ProductCard } from '@shared/components/productCard';
 import CustomLoading from '@shared/ui-kit/Loading/CustomLoading.vue';
+import { useFilterStore } from '../store/filterStore';
 
 const productStore = useProductStore();
+const filterStore = useFilterStore();
 
 onMounted(() => {
   productStore.requestGetProductByQueryParams();
@@ -28,6 +30,31 @@ onMounted(() => {
       :sizes="product.sizeValues"
     >
     </ProductCard>
+    <nav aria-label="Page navigation example">
+      <ul class="inline-flex h-10 -space-x-px text-base">
+        <li
+          class="ms-0 flex h-10 cursor-pointer items-center justify-center rounded-s-lg border border-e-0 border-gray-300 bg-white px-4 leading-tight text-gray-500 hover:bg-gray-100 hover:text-gray-700 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+        >
+          Previous
+        </li>
+        <li
+          v-for="num in productStore.GetTotalPaginationNumber"
+          :class="
+            productStore.GetCurrentPaginationNum + 1 === num
+              ? 'flex h-10 items-center justify-center border border-gray-300 bg-blue-50 px-4 text-blue-600 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white'
+              : 'flex h-10 cursor-pointer items-center justify-center border border-gray-300 bg-white px-4 leading-tight text-gray-500 hover:bg-gray-100 hover:text-gray-700 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white'
+          "
+          :key="num"
+        >
+          {{ num === productStore.GetCurrentPaginationNum + 1 }}
+        </li>
+        <li
+          class="flex h-10 cursor-pointer items-center justify-center rounded-e-lg border border-gray-300 bg-white px-4 leading-tight text-gray-500 hover:bg-gray-100 hover:text-gray-700 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+        >
+          Next
+        </li>
+      </ul>
+    </nav>
   </section>
   <CustomLoading v-else />
 </template>
